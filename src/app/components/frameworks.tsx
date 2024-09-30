e07e2ccdce066352c49c95a090f0db4e8fdb8c34
Jeff Campbell <jeffcampbell333@gmail.com>
2024-09-30 12:42:34 -0700

    Added frameworks component

--- src/app/components/frameworks.tsx
+++ src/app/components/frameworks.tsx
@@ -1,13 +1,22 @@
-import { SiReact, 
-    SiFlutter, 
-    SiTailwindcss, 
-    SiDotnet, 
-    SiNodedotjs,
-    SiNextdotjs,
-    
+import {
+  SiReact,
+  SiFlutter,
+  SiTailwindcss,
+  SiDotnet,
+  SiNodedotjs,
+  SiNextdotjs,
 } from "react-icons/si";
 import LangIcon from "./langicon";
 
-export default function Frameworks(){
-
+export default function Frameworks() {
+  return (
+    <div>
+        <LangIcon icon={<SiReact />} title="React.js"/>
+        <LangIcon icon={<SiNodedotjs />} title="Node.js"/>
+        <LangIcon icon={<SiNextdotjs />} title="Next.js"/>
+        <LangIcon icon={<SiTailwindcss />} title="TailwindCSS"/>
+        <LangIcon icon={<SiDotnet />} title=".Net"/>
+        <LangIcon icon={<SiFlutter />} title="Flutter.js"/>
+    </div>
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Languages from "./components/languages";
+import Frameworks from "./components/frameworks";
 
 export default function Home() {
   return (
     <div>
       <main>
         <Languages />
-        
+        <Frameworks />
       </main>
     </div>
   );
