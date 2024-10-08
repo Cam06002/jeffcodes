3905b5df5edf7c1f2f3bbbba1698eb15eb7bb344
Jeff Campbell <jeffcampbell333@gmail.com>
2024-10-08 14:36:38 -0700

    updated hero card

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -3,16 +3,13 @@ import Codeagon from "../../../public/images/codeagon.svg";
 
 export default function Hero(){
   return(
-    <div className="bg-gradient-to-r from-sky-300 to-red-300 w-full h-96 p-12 flex flex-row mb-8">
+    <div className="bg-gradient-to-r from-sky-300 to-red-300 w-full h-80 p-12 flex flex-row mb-8 justify-between">
       <div className="flex flex-col">
-        <h1 className="bg-gray-900/50 w-fit p-2">Hello, I'm Jeff Campbell</h1>
-        <p>Thank you for visiting my website</p>
-        <p>See my skills, work, and experience below,
-          click the envelope in the header to contact me,
-          or visit the links in the footer to see more!
-        </p>
+        <h1 className="text-gray-900/50 w-fit p-2 text-3xl mt-8 mb-4 font-bold">Hello, I'm Jeff Campbell</h1>
+        <h3 className="text-gray-900/50 w-fit p-2 text-xl font-bold">Software Engineer and Web Developer</h3>
+        <p className="text-gray-900/50 w-fit p-2">Focusing on reaching across the stack to produce performative web apps</p>
       </div>
-      <div className="flex flex-col justify-center align-middle ml-24 w-96">
+      <div className="flex flex-col align-middle ml-24">
         <Image
           src={Codeagon}
           className="h-48 w-48 flex justify-center"
