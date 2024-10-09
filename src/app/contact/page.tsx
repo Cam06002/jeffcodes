8833b285258cbed44446e1809e1611e1edb2364b
Jeff Campbell <jeffcampbell333@gmail.com>
2024-10-09 15:32:36 -0700

    added email sending with nodemailer

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,33 +1,63 @@
+'use client';
+
+import { FC } from 'react';
+import { useForm } from 'react-hook-form';
+import SendEmail from './sendEmail';
 
 
-export default function Contact(){
+export type FormData = {
+  subject: string;
+  email: string;
+  message: string;
+};
+
+const Contact: FC = () => {
+  const { register, handleSubmit } = useForm<FormData>();
+
+  function onSubmit(data: FormData) {
+    SendEmail(data);
+  }
+
   return(
     <div className="w-full h-full p-12 flex flex-col">
       <h3 className="text-align-center text-3xl font-bold mb-8">Contact</h3>
       <form 
         className="flex flex-col"
+        onSubmit={handleSubmit(onSubmit)}
       >
         <div className="flex md:flex-row align-middle my-2">
           <label 
             className="mr-2 w-24"
             htmlFor="email"
           >Your Email</label>
-          <input className="rounded text-black w-60 p-1" type="text" id="email"/>
+          <input 
+            className="rounded text-black w-60 p-1" 
+            type="text" id="email"
+            {...register('email', {required: true})}
+          />
         </div>
         <div className="flex md:flex-row content-center my-2">
           <label 
             className="mr-2 w-24"
             htmlFor="subject"
           >Subject</label>
-          <input className="rounded w-60 text-black p-1" type="text" id="subject"/>
+          <input 
+            className="rounded w-60 text-black p-1" 
+            type="text" id="subject"
+            {...register('subject', {required: true})}
+          />
         </div>
         <label
           className="mt-4 mb-2"
           htmlFor="message"
         >Your Message</label>
-        <textarea className="rounded p-2 text-black" id="message" rows={12}/>
+        <textarea className="rounded p-2 text-black" id="message" rows={12}
+          {...register('message', {required: true})}
+        />
         <button className="bg-sky-300 font-bold text-black shadow w-fit mt-4 p-2 rounded">Submit</button>
       </form>
     </div>
   )
 }
+
+export default Contact;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,27 +7,21 @@ import Hero from "./components/hero";
 
 export default function Home() {
   return (
-    <div
-      className="[&::-webkit-scrollbar]:[width:10px]
-            [&::-webkit-scrollbar-thumb]:bg-sky-300
-            overflow-scroll"
-    >
-      <main className="font-Orbitron flex flex-col w-full">
-        <Hero />
-        <div className="flex flex-col max-w-full md:flex-row">
-          <div className="flex flex-row flex-wrap md:flex-col md:justify-start justify-center border-r-2 w-fit">
-            <Languages />
-            <Frameworks />
-            <Cloud />
-          </div>
-          <div className="flex md:flex-col border-r-2 md:w-1/2">
-            <Projects />
-          </div>
-          <div className="flex md:flex-col">
-            <Experience />
-          </div>
+    <main className="font-Orbitron flex flex-col w-full">
+      <Hero />
+      <div className="flex flex-col max-w-full md:flex-row">
+        <div className="flex flex-row flex-wrap md:flex-col md:justify-start justify-center border-r-2 w-fit">
+          <Languages />
+          <Frameworks />
+          <Cloud />
         </div>
-      </main>
-    </div>
+        <div className="flex md:flex-col border-r-2 md:w-1/2">
+          <Projects />
+        </div>
+        <div className="flex md:flex-col">
+          <Experience />
+        </div>
+      </div>
+    </main>
   );
 }
