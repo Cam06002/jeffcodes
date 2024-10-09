ffb105bbd3cd557888cf2f81e31e0925da1d7eb1
Jeff Campbell <jeffcampbell333@gmail.com>
2024-10-09 16:40:52 -0700

    header sizing

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -6,9 +6,9 @@ export default function Hero(){
   return(
     <div className="bg-gradient-to-r from-sky-300 to-red-300 w-full h-fit p-12 flex flex-row mb-8 justify-between">
       <div className="flex flex-col">
-        <h1 className="text-gray-900/50 w-fit p-2 text-3xl mt-8 mb-4 font-bold">Hello, I am Jeff Campbell</h1>
-        <h3 className="text-gray-900/50 w-fit p-2 text-xl font-bold">Software Engineer and Web Developer</h3>
-        <p className="text-gray-900/50 w-fit p-2">Focusing on reaching across the stack to produce performative web apps</p>
+        <h1 className="text-gray-900/50 w-fit p-2 text-xl md:text-3xl mt-8 mb-4 font-bold">Jeff Campbell</h1>
+        <h3 className="text-gray-900/50 w-fit p-2 md:text-xl font-bold">Software Engineer and Web Developer</h3>
+        <p className="text-gray-900/50 w-fit text-xs md:text-md p-2">Focusing on reaching across the stack to produce performative web apps</p>
         <Link 
           className="bg-red-300 shadow-xl hover:shadow-3xl rounded text-sky-200 w-fit p-2 m-4 font-bold"
           href={"./contact"}
@@ -19,7 +19,7 @@ export default function Hero(){
       <div className="flex flex-col align-middle ml-24">
         <Image
           src={Codeagon}
-          className="h-48 w-48 flex justify-center"
+          className="h-20 w-20 md:h-48 md:w-48 flex justify-center"
           alt="code brack in a hexagon"
           priority={false}
         />
