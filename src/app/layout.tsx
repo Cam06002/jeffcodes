86e037f4ce30fba16f9b4cf76afe2b8e1983c578
Jeff Campbell <jeffcampbell333@gmail.com>
2024-10-10 13:24:27 -0700

    moved head to page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,15 +16,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <title>Jeff Campbell - Portfolio</title>
-        <meta
-          name="description"
-          content="Check out the portfolio website for Jeff Campbell!"
-          key="desc"
-        />
-      </Head>
       <body className="flex flex-col h-screen w-screen bg-black">
         <Header />
         <main className='flex flex-row flex-grow m-2 max-w-full max-h-full rounded overflow-auto [&::-webkit-scrollbar]:[width:10px]
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 import Languages from "./components/languages";
 import Frameworks from "./components/frameworks";
 import Cloud from "./components/cloud";
@@ -8,6 +10,15 @@ import Hero from "./components/hero";
 export default function Home() {
   return (
     <main className="font-Orbitron flex flex-col w-full">
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <title>Jeff Campbell - Portfolio</title>
+        <meta
+          name="description"
+          content="Check out the portfolio website for Jeff Campbell!"
+          key="desc"
+        />
+      </Head>
       <Hero />
       <div className="flex flex-col max-w-full md:flex-row">
         <div className="flex flex-row flex-wrap md:flex-col md:justify-start justify-center border-r-2 w-fit">
